84c9a4119236a5b3aa9a367153dda4ae7979333e
Giovanna G B Fritsche <ggarciabas@gmail.com>
2023-05-16 08:34:14 -0300

    feat: mpv

---
+++ src/components/App.tsx
@@ -0,0 +1,27 @@
+import { MantineProvider } from "@mantine/styles";
+import { theme } from "../utils/theme";
+import { CustomFonts } from "../fonts/CustomFonts";
+import { AppShell, Center, Title } from "@mantine/core";
+import { HeaderResponsive } from "./HeaderResponsive";
+import Linkable from "./Linkable";
+import Enrollment from "./Enrollment";
+import Banner from "./Banner";
+import { About } from "./About";
+
+export default function App() {
+  document.title = "MANOBRAS PARA VIDA";
+  return (
+    <MantineProvider theme={{ ...theme }} withGlobalStyles withNormalizeCSS>
+      <CustomFonts />
+      <AppShell header={<HeaderResponsive />} padding={0}>
+        <Banner />
+        <Linkable id="inscricao">
+          <Enrollment />
+        </Linkable>
+        <Linkable id="sobre">
+          <About />
+        </Linkable>
+      </AppShell>
+    </MantineProvider>
+  );
+}
